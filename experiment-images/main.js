4f97d921452379e95c1476820c63754eae1868b8
Frederik De Bleser <frederik@debleser.be>
2020-12-18 16:55:54 +0100

    Lots of experiments!

---
+++ experiment-images/main.js
@@ -0,0 +1,25 @@
+import "regenerator-runtime/runtime";
+import PDFDocument from "pdfkit";
+import blobStream from "blob-stream";
+import { loadImage } from "../utils/image";
+
+async function makePdf() {
+  const iframe = document.querySelector("iframe");
+  const doc = new PDFDocument({
+    size: [421, 595],
+  });
+
+  const stream = doc.pipe(blobStream());
+
+  const image = await loadImage("/images/road.jpg");
+  doc.image(image, 0, 15, { width: 300 });
+  doc.image(image, 0, 100, { width: 512 });
+
+  // end and display the document in the iframe to the right
+  doc.end();
+  stream.on("finish", function () {
+    iframe.src = stream.toBlobURL("application/pdf");
+  });
+}
+
+makePdf();
