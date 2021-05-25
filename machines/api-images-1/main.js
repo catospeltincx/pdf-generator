a49c042550e6b142246246005a39999352369f9e
catospeltincx <cato.speltincx@telenet.be>
2021-05-25 10:36:27 +0200

    nota all-images & generate-image-book

--- machines/api-images-1/main.js
+++ machines/api-images-1/main.js
@@ -17,15 +17,22 @@ function downloadText(text, filename) {
   document.body.removeChild(a);
 }
 
+//geen dubbele images van de verschillende fetches in de lijst hebben
+//als die er al is, overslaan
 const seenImages = new Set();
 
+// een ljson lijst maken van al die images
 async function loadImagesForPages(pages) {
   console.log(pages);
   const allImageObjects = [];
+  //voor elke pagina alle images loaden
   for (const pageName of pages) {
     const imageObjects = await loadImagesForPage(pageName);
+    //om lijst in lijst te vermijden
+    //steek de elementen van de lijst erin
     allImageObjects.push(...imageObjects);
   }
+  //verander hier de file name
   downloadText(JSON.stringify(allImageObjects), "all-images.json");
 }
 
@@ -50,24 +57,35 @@ async function loadImagesForPage(pageName, count = 100) {
   // Get all thumbs
   let thumbs = Array.from(div.querySelectorAll(".gallerybox"));
   console.log(thumbs);
+
+  //alle images met captions bij
   for (const thumb of thumbs) {
+    //alle images aanduiden
     const image = thumb.querySelector("img");
+    //als er geen image is in een gallery box --> skip
     if (!image) continue;
     if (seenImages.has(image.src)) continue;
     seenImages.add(image.src);
+    //get the captions
     const captionDiv = thumb.querySelector(".gallerytext");
+    //kijken of die caption div echt bestaat
+    //en daar dan de caption uithalen
     let caption = "";
     if (captionDiv) {
       caption = captionDiv.textContent;
     }
+
+    //opzoek naar het grid
     const grid = document.querySelector(".grid");
+    //per beeld een div
     const div = document.createElement("div");
+    //naam div die we in css dus kunnen aanspreken
     div.className = "image-with-caption";
     div.appendChild(image);
     const span = document.createElement("span");
     span.textContent = caption;
     div.appendChild(span);
-
+    //alle gegevens die we van die image bijhouden
     grid.appendChild(div);
     imageObjects.push({
       src: image.src,
@@ -76,9 +94,15 @@ async function loadImagesForPage(pageName, count = 100) {
     });
   }
 
+  //ook alle images die we ervoor overgeslagen hebben
+  //die geen captions hebben
+  //ALLE images,
+  //Ook icoontjes enzo
   let images = Array.from(div.querySelectorAll("img"));
   for (const image of images) {
+    //de images die we al gezien hebben niet dubbel nemen
     if (seenImages.has(image.src)) continue;
+    //toevoegen aan de lijst van images die we al gemaakt hebben
     seenImages.add(image.src);
     const grid = document.querySelector(".grid");
     grid.appendChild(image);
@@ -94,15 +118,21 @@ async function loadImagesForPage(pageName, count = 100) {
   //   json.push(pageName);
   // }
   // downloadText(JSON.stringify(json), "page-links.json");
+
+  //
   return imageObjects;
 }
 
+// een input veld, search bar die vertrekt vanaf een file
 document.getElementById("file").addEventListener("change", (e) => {
+  //files 'toelaten'
+  //wij laten er hier maar eentje toe
+  //haalt de files eruit
   const file = e.target.files[0];
-  console.log(file);
-  console.log(imageUrl);
-  console.log("input");
-  console.log(e);
+  // console.log(file);
+  // console.log(imageUrl);
+  // console.log("input");
+  // console.log(e);
   const imageUrl = URL.createObjectURL(file);
   fetch(imageUrl)
     .then((res) => res.json())
--- machines/generate-image-book/main.js
+++ machines/generate-image-book/main.js
@@ -11,11 +11,14 @@ async function makePdf(images) {
 
   const stream = doc.pipe(blobStream());
 
+  //
   //.slice om niet alles te weergeven
-  for (const imageObject of images.slice(0, 1000)) {
+  for (const imageObject of images.slice(0, 100)) {
     const image = await loadImage(imageObject.src);
-    doc.image(image, { width: 300 });
-    doc.text(imageObject.caption);
+    //wanneer x en y meegegeven, blijven ze plakken
+    doc.image(image, 0, 15, { width: 300 });
+    //de caption
+    doc.text(imageObject.caption, { width: 100 });
   }
 
   // end and display the document in the iframe to the right
@@ -25,9 +28,14 @@ async function makePdf(images) {
   });
 }
 
+//dealen met json lijst images
+//
 document.getElementById("file").addEventListener("change", (e) => {
+  //we nemen de file
   const file = e.target.files[0];
+  //de data van de json
   const jsonUrl = URL.createObjectURL(file);
+  //als de json geladen is, is er een lijst van alle images
   fetch(jsonUrl)
     .then((res) => res.json())
     .then((json) => makePdf(json));
